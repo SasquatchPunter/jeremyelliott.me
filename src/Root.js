00e97c0014dac21711d81c322fdf0612689c989a
Jeremy Elliott <jodomoi64@yahoo.ca>
2022-09-11 21:15:16 -0600

    refactor: cleanup

---
+++ src/Root.js
@@ -0,0 +1,16 @@
+function Root() {
+  return (
+    <div id="site-container">
+      <h1>JEREMYELLIOTT.ME</h1>
+      <p>
+        Hi! I'm Jeremy, a web developer / designer based in Canmore, Alberta
+      </p>
+      <p>
+        Obviously there's not a lot to see just yet, but check back often while
+        I build out the site!
+      </p>
+    </div>
+  );
+}
+
+export default Root;
